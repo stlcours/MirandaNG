b73c7c51839a6255831fb9b4d4143e08a816181f
Alexander Lantsev <aunsane@gmail.com>
2015-04-27 20:18:40 +0000

    EmLan: - started moving to unicode - started moving to multiacc - unified project
    git-svn-id: http://svn.miranda-ng.org/main/branches/unsane@13207 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

---
+++ EmLan/EmLanProto/src/resource.h
@@ -0,0 +1,22 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by amdproto.rc
+//
+#define IDI_ICON_ONLINE                 101
+#define IDI_ICON_OFFLINE                103
+#define IDD_EMP_FORM_OPT                104
+#define IDC_LIST_IP                     1001
+#define IDC_RADIO_USECOMPNAME           1004
+#define IDC_RADIO_USEOWN                1005
+#define IDC_EDIT_NAME                   1006
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1008
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
