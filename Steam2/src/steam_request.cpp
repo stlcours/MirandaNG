c20dab276c7cef498b6429eb636b771bd7dd93f6
Alexander Lantsev <aunsane@gmail.com>
2015-05-14 10:40:43 +0000

    git-svn-id: http://svn.miranda-ng.org/main/branches/unsane@13583 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

---
+++ Steam2/src/steam_request.cpp
@@ -0,0 +1,66 @@
+#include "stdafx.h"
+
+class SteamResponseDelegate
+{
+private:
+	CSteamProto *proto;
+	SteamResponseCallback responseCallback;
+	SteamResponseWithArgCallback responseWithArgCallback;
+
+	void *arg;
+	bool hasArg;
+
+public:
+	SteamResponseDelegate(CSteamProto *proto, SteamResponseCallback responseCallback)
+		: proto(proto), responseCallback(responseCallback), responseWithArgCallback(NULL), arg(NULL), hasArg(false) {}
+
+	SteamResponseDelegate(CSteamProto *proto, SteamResponseWithArgCallback responseCallback, void *arg)
+		: proto(proto), responseCallback(NULL), responseWithArgCallback(responseCallback), arg(arg), hasArg(true) { }
+
+	void Invoke(const NETLIBHTTPREQUEST *response)
+	{
+		if (hasArg)
+			(proto->*(responseWithArgCallback))(response, arg);
+		else
+			(proto->*(responseCallback))(response);
+	}
+};
+
+static void SteamHttpResponse(const NETLIBHTTPREQUEST *response, void *arg)
+{
+	((SteamResponseDelegate*)arg)->Invoke(response);
+}
+
+void CSteamProto::PushRequest(HttpRequest *request)
+{
+	requestQueue->Push(request);
+}
+
+void CSteamProto::PushRequest(HttpRequest *request, SteamResponseCallback response)
+{
+	SteamResponseDelegate *delegate = new SteamResponseDelegate(this, response);
+	requestQueue->Push(request, SteamHttpResponse, delegate);
+}
+
+void CSteamProto::PushRequest(HttpRequest *request, SteamResponseWithArgCallback response, void *arg, HttpFinallyCallback last)
+{
+	SteamResponseDelegate *delegate = new SteamResponseDelegate(this, response, arg);
+	requestQueue->Push(request, SteamHttpResponse, delegate, last);
+}
+
+void CSteamProto::SendRequest(HttpRequest *request)
+{
+	requestQueue->Send(request, NULL, NULL);
+}
+
+void CSteamProto::SendRequest(HttpRequest *request, SteamResponseCallback response)
+{
+	SteamResponseDelegate *delegate = new SteamResponseDelegate(this, response);
+	requestQueue->Send(request, SteamHttpResponse, delegate);
+}
+
+void CSteamProto::SendRequest(HttpRequest *request, SteamResponseWithArgCallback response, void *arg, HttpFinallyCallback last)
+{
+	SteamResponseDelegate *delegate = new SteamResponseDelegate(this, response, arg);
+	requestQueue->Send(request, SteamHttpResponse, delegate, last);
+}
