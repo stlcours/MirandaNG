88a3af869b823767e319d7456ab9eaf6ecd421a0
Alexander Lantsev <aunsane@gmail.com>
2015-05-14 11:41:06 +0000

    Steam: cleanup
    git-svn-id: http://svn.miranda-ng.org/main/branches/unsane@13584 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- Steam2/src/steam_request.cpp
+++ Steam2/src/steam_request.cpp
@@ -6,21 +6,26 @@ private:
 	CSteamProto *proto;
 	SteamResponseCallback responseCallback;
 	SteamResponseWithArgCallback responseWithArgCallback;
+	HttpFinallyCallback httpFinallyCallback;
 
 	void *arg;
 	bool hasArg;
 
 public:
 	SteamResponseDelegate(CSteamProto *proto, SteamResponseCallback responseCallback)
-		: proto(proto), responseCallback(responseCallback), responseWithArgCallback(NULL), arg(NULL), hasArg(false) {}
+		: proto(proto), responseCallback(responseCallback), responseWithArgCallback(NULL), arg(NULL), httpFinallyCallback(NULL), hasArg(false) {}
 
-	SteamResponseDelegate(CSteamProto *proto, SteamResponseWithArgCallback responseCallback, void *arg)
-		: proto(proto), responseCallback(NULL), responseWithArgCallback(responseCallback), arg(arg), hasArg(true) { }
+	SteamResponseDelegate(CSteamProto *proto, SteamResponseWithArgCallback responseCallback, void *arg, HttpFinallyCallback httpFinallyCallback)
+		: proto(proto), responseCallback(NULL), responseWithArgCallback(responseCallback), arg(arg), httpFinallyCallback(httpFinallyCallback), hasArg(true) { }
 
 	void Invoke(const NETLIBHTTPREQUEST *response)
 	{
 		if (hasArg)
+		{
 			(proto->*(responseWithArgCallback))(response, arg);
+			if (httpFinallyCallback != NULL)
+				httpFinallyCallback(arg);
+		}
 		else
 			(proto->*(responseCallback))(response);
 	}
@@ -28,7 +33,9 @@ public:
 
 static void SteamHttpResponse(const NETLIBHTTPREQUEST *response, void *arg)
 {
-	((SteamResponseDelegate*)arg)->Invoke(response);
+	SteamResponseDelegate *delegate = (SteamResponseDelegate*)arg;
+	delegate->Invoke(response);
+	delete delegate;
 }
 
 void CSteamProto::PushRequest(HttpRequest *request)
@@ -44,8 +51,8 @@ void CSteamProto::PushRequest(HttpRequest *request, SteamResponseCallback respon
 
 void CSteamProto::PushRequest(HttpRequest *request, SteamResponseWithArgCallback response, void *arg, HttpFinallyCallback last)
 {
-	SteamResponseDelegate *delegate = new SteamResponseDelegate(this, response, arg);
-	requestQueue->Push(request, SteamHttpResponse, delegate, last);
+	SteamResponseDelegate *delegate = new SteamResponseDelegate(this, response, arg, last);
+	requestQueue->Push(request, SteamHttpResponse, delegate);
 }
 
 void CSteamProto::SendRequest(HttpRequest *request)
@@ -61,6 +68,6 @@ void CSteamProto::SendRequest(HttpRequest *request, SteamResponseCallback respon
 
 void CSteamProto::SendRequest(HttpRequest *request, SteamResponseWithArgCallback response, void *arg, HttpFinallyCallback last)
 {
-	SteamResponseDelegate *delegate = new SteamResponseDelegate(this, response, arg);
-	requestQueue->Send(request, SteamHttpResponse, delegate, last);
+	SteamResponseDelegate *delegate = new SteamResponseDelegate(this, response, arg, last);
+	requestQueue->Send(request, SteamHttpResponse, delegate);
 }
