stlcours/MirandaNG

commit 82c335a9e6361f021f24890fc147f046ac553e9b

warning fixes
git-svn-id: http://svn.miranda-ng.org/main/trunk@14980 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

plugins/Clist_modern/src/modern_clcpaint.cpp
@@ -663,7 +663,6 @@ void CLCPaint::_PaintRowItemsEx(HWND hwnd, HDC hdcMem, ClcData *dat, ClcContact 
 	// Let calc placeholder
 	int minheight = dat->row_min_heigh;
 	int mode2 = -1;
-	COLORREF colourFg = RGB(0, 0, 0);
 	BOOL InClistWindow = (dat->hWnd == pcli->hwndContactTree);
 	ClcCacheEntry *pdnce = NULL;
 	int height = RowHeight_CalcRowHeight(dat, hwnd, Drawing, -1);
@@ -779,7 +778,7 @@ void CLCPaint::_PaintRowItemsEx(HWND hwnd, HDC hdcMem, ClcData *dat, ClcContact 
 		//3 draw text
 		{
 			SIZE text_size = { 0 };
-			TCHAR * szCounts = NULL;
+			TCHAR *szCounts = NULL;
 			RECT text_rect = fr_rc;
 			RECT counts_rc = { 0 };
 			UINT uTextFormat = DT_LEFT | DT_VCENTER | (gl_TrimText ? DT_END_ELLIPSIS : 0) | DT_SINGLELINE;
@@ -797,7 +796,7 @@ void CLCPaint::_PaintRowItemsEx(HWND hwnd, HDC hdcMem, ClcData *dat, ClcContact 
 				RECT count_rc = { 0 };
 				SIZE count_size = { 0 };
 				int space_width = 0;
-				TCHAR * szCounts = pcli->pfnGetGroupCountsText(dat, Drawing);
+				szCounts = pcli->pfnGetGroupCountsText(dat, Drawing);
 				// Has to draw the count?
 				if (szCounts && mir_tstrlen(szCounts) > 0) {
 					// calc width and height
@@ -813,16 +812,16 @@ void CLCPaint::_PaintRowItemsEx(HWND hwnd, HDC hdcMem, ClcData *dat, ClcContact 
 				}
 				// modify text rect
 				{
-					SIZE text_size = { 0 };
+					SIZE grp_size = { 0 };
 					int wid = fr_rc.right - fr_rc.left;
 					ChangeToFont(hdcMem, dat, Drawing->group->expanded ? FONTID_OPENGROUPS : FONTID_CLOSEDGROUPS, NULL);
-					GetTextSize(&text_size, hdcMem, fr_rc, Drawing->szText, Drawing->ssText.plText, 0, dat->text_resize_smileys ? 0 : Drawing->ssText.iMaxSmileyHeight);
+					GetTextSize(&grp_size, hdcMem, fr_rc, Drawing->szText, Drawing->ssText.plText, 0, dat->text_resize_smileys ? 0 : Drawing->ssText.iMaxSmileyHeight);
 
-					if (wid - count_size.cx > text_size.cx) {
+					if (wid - count_size.cx > grp_size.cx) {
 						if (dat->row_align_group_mode != 2) { //center or left
-							int x = (dat->row_align_group_mode == 1) ? (wid - (text_size.cx + count_size.cx)) >> 1 : 0;
+							int x = (dat->row_align_group_mode == 1) ? (wid - (grp_size.cx + count_size.cx)) >> 1 : 0;
 							nameRect.left += x;
-							nameRect.right = nameRect.left + text_size.cx;
+							nameRect.right = nameRect.left + grp_size.cx;
 							countRect.left = nameRect.right + space_width;
 							countRect.right = countRect.left + count_size.cx - space_width;
 						}
@@ -830,7 +829,7 @@ void CLCPaint::_PaintRowItemsEx(HWND hwnd, HDC hdcMem, ClcData *dat, ClcContact 
 							countRect.right = nameRect.right;
 							countRect.left = countRect.right - ((count_size.cx > 0) ? (count_size.cx - space_width) : 0);
 							nameRect.right = countRect.left - ((count_size.cx > 0) ? space_width : 0);
-							nameRect.left = nameRect.right - text_size.cx;
+							nameRect.left = nameRect.right - grp_size.cx;
 						}
 					}
 					else {
@@ -1042,16 +1041,16 @@ void CLCPaint::_PaintRowItemsEx(HWND hwnd, HDC hdcMem, ClcData *dat, ClcContact 
 						}
 						// modify text rect
 						{
-							SIZE text_size = { 0 };
+							SIZE grp_size = { 0 };
 							int wid = p_rect.right - p_rect.left;
 							ChangeToFont(hdcMem, dat, Drawing->group->expanded ? FONTID_OPENGROUPS : FONTID_CLOSEDGROUPS, NULL);
-							GetTextSize(&text_size, hdcMem, p_rect, Drawing->szText, Drawing->ssText.plText, 0, dat->text_resize_smileys ? 0 : Drawing->ssText.iMaxSmileyHeight);
+							GetTextSize(&grp_size, hdcMem, p_rect, Drawing->szText, Drawing->ssText.plText, 0, dat->text_resize_smileys ? 0 : Drawing->ssText.iMaxSmileyHeight);
 
-							if (wid - count_size.cx > text_size.cx) {
+							if (wid - count_size.cx > grp_size.cx) {
 								if (dat->row_align_group_mode != 2) { //center or left
-									int x = (dat->row_align_group_mode == 1) ? (wid - (text_size.cx + count_size.cx)) >> 1 : 0;
+									int x = (dat->row_align_group_mode == 1) ? (wid - (grp_size.cx + count_size.cx)) >> 1 : 0;
 									nameRect.left += x;
-									nameRect.right = nameRect.left + text_size.cx;
+									nameRect.right = nameRect.left + grp_size.cx;
 									countRect.left = nameRect.right + space_width;
 									countRect.right = countRect.left + count_size.cx - space_width;
 								}
@@ -1059,7 +1058,7 @@ void CLCPaint::_PaintRowItemsEx(HWND hwnd, HDC hdcMem, ClcData *dat, ClcContact 
 									countRect.right = nameRect.right;
 									countRect.left = countRect.right - ((count_size.cx > 0) ? (count_size.cx - space_width) : 0);
 									nameRect.right = countRect.left - ((count_size.cx > 0) ? space_width : 0);
-									nameRect.left = nameRect.right - text_size.cx;
+									nameRect.left = nameRect.right - grp_size.cx;
 								}
 							}
 							else {
@@ -1235,13 +1234,12 @@ void CLCPaint::_PaintRowItemsEx(HWND hwnd, HDC hdcMem, ClcData *dat, ClcContact 
 					}
 					else {
 						HRGN rgn = NULL;
-						HRGN oldrgn;
 						int round_radius = 0;
-						int width = p_rect.right - p_rect.left;
-						int height = p_rect.bottom - p_rect.top;
+						int ava_width = p_rect.right - p_rect.left;
+						int ava_height = p_rect.bottom - p_rect.top;
 						// Store pos
 						Drawing->pos_avatar = p_rect;
-						oldrgn = CreateRectRgn(0, 0, 0, 0);
+						HRGN oldrgn = CreateRectRgn(0, 0, 0, 0);
 						GetClipRgn(hdcMem, oldrgn);
 
 						// Round corners
@@ -1249,7 +1247,7 @@ void CLCPaint::_PaintRowItemsEx(HWND hwnd, HDC hdcMem, ClcData *dat, ClcContact 
 							if (dat->avatars_use_custom_corner_size)
 								round_radius = dat->avatars_custom_corner_size;
 							else
-								round_radius = min(width, height) / 5;
+								round_radius = min(ava_width, ava_height) / 5;
 						}
 						else round_radius = 0;
 
@@ -1336,11 +1334,10 @@ void CLCPaint::_PaintRowItemsEx(HWND hwnd, HDC hdcMem, ClcData *dat, ClcContact 
 					(!Drawing->isSubcontact || dat->dbbMetaHideExtra == 0 && dat->extraColumnsCount > 0)) {
 					int BlendedInActiveState = dat->dbbBlendInActiveState;
 					int BlendValue = dat->dbbBlend25 ? ILD_BLEND25 : ILD_BLEND50;
-					int iImage;
 					int count = 0;
 					RECT rc;
 					int x = 0;
-					for (iImage = 0; iImage < dat->extraColumnsCount; iImage++) {
+					for (int iImage = 0; iImage < dat->extraColumnsCount; iImage++) {
 						COLORREF colourFg = dat->selBkColour;
 						int mode = BlendedInActiveState ? BlendValue : ILD_NORMAL;
 						if (Drawing->iExtraImage[iImage] == EMPTY_EXTRA_ICON) {
@@ -1390,6 +1387,7 @@ void CLCPaint::_PaintRowItemsEx(HWND hwnd, HDC hdcMem, ClcData *dat, ClcContact 
 					int eNum = gl_RowTabAccess[i]->type - TC_EXTRA1;
 					if (eNum < dat->extraColumnsCount) {
 						if (Drawing->iExtraImage[eNum] != EMPTY_EXTRA_ICON) {
+							COLORREF colourFg = RGB(0, 0, 0);
 							int mode = 0;
 							int BlendedInActiveState = dat->dbbBlendInActiveState;
 							int BlendValue = dat->dbbBlend25 ? ILD_BLEND25 : ILD_BLEND50;
@@ -2024,7 +2022,6 @@ void CLCPaint::_CalcItemsPos(HDC hdcMem, ClcData *dat, ClcContact *Drawing, RECT
 		switch (dat->row_items[item]) {
 		case ITEM_AVATAR: ///////////////////////////////////////////////////////////////////////////////////////////////////
 			{
-				RECT rc;
 				int max_width;
 				int width;
 				int height;
@@ -2044,14 +2041,10 @@ void CLCPaint::_CalcItemsPos(HDC hdcMem, ClcData *dat, ClcContact *Drawing, RECT
 
 					// Has to draw icon instead?
 					if (dat->icon_hide_on_avatar && dat->icon_draw_on_avatar_space && Drawing->iImage != -1) {
-						RECT rc;
-
 						// Make rectangle
-						rc = _GetRectangle(dat, &row_rc, &free_row_rc, &left_pos, &right_pos,
-							left, dat->iconXSpace, max_width, ICON_HEIGHT, HORIZONTAL_SPACE);
-
+						RECT rc = _GetRectangle(dat, &row_rc, &free_row_rc, &left_pos, &right_pos, left, dat->iconXSpace, max_width, ICON_HEIGHT, HORIZONTAL_SPACE);
 						if (rc.left < rc.right) {
-							/* center icon in avatar place */
+							// center icon in avatar place
 							if (rc.right - rc.left > 16) rc.left += (((rc.right - rc.left) - 16) >> 1);
 							if (rc.bottom - rc.top > 16) rc.top += (((rc.bottom - rc.top) - 16) >> 1);
 
@@ -2063,8 +2056,7 @@ void CLCPaint::_CalcItemsPos(HDC hdcMem, ClcData *dat, ClcContact *Drawing, RECT
 						// Has to keep the empty space??
 						if ((left && !dat->row_align_left_items_to_left) || (!left && !dat->row_align_right_items_to_right)) {
 							// Make rectangle
-							rc = _GetRectangle(dat, &row_rc, &free_row_rc, &left_pos, &right_pos,
-								left, max_width, max_width, dat->avatars_maxheight_size, HORIZONTAL_SPACE);
+							// RECT rc = _GetRectangle(dat, &row_rc, &free_row_rc, &left_pos, &right_pos, left, max_width, max_width, dat->avatars_maxheight_size, HORIZONTAL_SPACE);
 
 							// Store position
 							//StoreItemPos( Drawing, CIT_AVATAR, &rc );
@@ -2089,8 +2081,7 @@ void CLCPaint::_CalcItemsPos(HDC hdcMem, ClcData *dat, ClcContact *Drawing, RECT
 				}
 
 				// Make rectangle
-				rc = _GetRectangle(dat, &row_rc, &free_row_rc, &left_pos, &right_pos,
-					left, width, max_width, height, HORIZONTAL_SPACE);
+				RECT rc = _GetRectangle(dat, &row_rc, &free_row_rc, &left_pos, &right_pos, left, width, max_width, height, HORIZONTAL_SPACE);
 
 				rc.top = max(free_row_rc.top, rc.top);
 				rc.bottom = min(free_row_rc.bottom, rc.bottom);
@@ -2103,7 +2094,6 @@ void CLCPaint::_CalcItemsPos(HDC hdcMem, ClcData *dat, ClcContact *Drawing, RECT
 
 		case ITEM_ICON: /////////////////////////////////////////////////////////////////////////////////////////////////////
 			{
-				RECT rc;
 				int iImage = -1;
 				BOOL has_avatar = Drawing->avatar_data != NULL && !CheckMiniMode(dat, selected);
 
@@ -2114,13 +2104,9 @@ void CLCPaint::_CalcItemsPos(HDC hdcMem, ClcData *dat, ClcContact *Drawing, RECT
 					&& !Drawing->image_is_special) {
 					// Don't have to draw, but has to keep the empty space?
 					if ((left && !dat->row_align_left_items_to_left) || (!left && !dat->row_align_right_items_to_right)) {
-						rc = _GetRectangle(dat, &row_rc, &free_row_rc, &left_pos, &right_pos,
-							left, dat->iconXSpace, dat->iconXSpace, ICON_HEIGHT, HORIZONTAL_SPACE);
-
-						if (rc.left < rc.right) {
-							// Store position
+						RECT rc = _GetRectangle(dat, &row_rc, &free_row_rc, &left_pos, &right_pos, left, dat->iconXSpace, dat->iconXSpace, ICON_HEIGHT, HORIZONTAL_SPACE);
+						if (rc.left < rc.right) // Store position
 							_StoreItemPos(Drawing, CIT_ICON, &rc);
-						}
 					}
 					break;
 				}
@@ -2128,10 +2114,10 @@ void CLCPaint::_CalcItemsPos(HDC hdcMem, ClcData *dat, ClcContact *Drawing, RECT
 					&& dat->icon_hide_on_avatar
 					&& dat->icon_draw_on_avatar_space
 					&& (!Drawing->image_is_special || !has_avatar ||
-					(dat->avatars_draw_overlay
-					&& dat->avatars_maxheight_size >= ICON_HEIGHT + (dat->avatars_draw_border ? 2 : 0)
-					&& GetContactCachedStatus(Drawing->hContact) - ID_STATUS_OFFLINE < _countof(g_pAvatarOverlayIcons)
-					&& dat->avatars_overlay_type == SETTING_AVATAR_OVERLAY_TYPE_CONTACT))) {
+						(dat->avatars_draw_overlay
+							&& dat->avatars_maxheight_size >= ICON_HEIGHT + (dat->avatars_draw_border ? 2 : 0)
+							&& GetContactCachedStatus(Drawing->hContact) - ID_STATUS_OFFLINE < _countof(g_pAvatarOverlayIcons)
+							&& dat->avatars_overlay_type == SETTING_AVATAR_OVERLAY_TYPE_CONTACT))) {
 					// Don't have to draw and don't have to keep the empty space
 					break;
 				}
@@ -2146,13 +2132,9 @@ void CLCPaint::_CalcItemsPos(HDC hdcMem, ClcData *dat, ClcContact *Drawing, RECT
 				// Has image to draw?
 				if (iImage != -1) {
 					// Make rectangle
-					rc = _GetRectangle(dat, &row_rc, &free_row_rc, &left_pos, &right_pos,
-						left, dat->iconXSpace, dat->iconXSpace, ICON_HEIGHT, HORIZONTAL_SPACE);
-
-					if (rc.left < rc.right) {
-						// Store position
+					RECT rc = _GetRectangle(dat, &row_rc, &free_row_rc, &left_pos, &right_pos, left, dat->iconXSpace, dat->iconXSpace, ICON_HEIGHT, HORIZONTAL_SPACE);
+					if (rc.left < rc.right) // Store position
 						_StoreItemPos(Drawing, CIT_ICON, &rc);
-					}
 				}
 			}
 			break;
@@ -2164,23 +2146,19 @@ void CLCPaint::_CalcItemsPos(HDC hdcMem, ClcData *dat, ClcContact *Drawing, RECT
 					TCHAR szResult[80];
 
 					if (!TimeZone_PrintDateTime(pdnce->hTimeZone, _T("t"), szResult, _countof(szResult), 0)) {
-						SIZE text_size;
-						RECT rc;
-
 						// Select font
 						ChangeToFont(hdcMem, dat, FONTID_CONTACT_TIME, NULL);
 
 						// Get text size
+						RECT rc;
+						SIZE text_size;
 						text_size.cy = ske_DrawText(hdcMem, szResult, (int)mir_tstrlen(szResult), &rc, DT_CALCRECT | DT_NOPREFIX | DT_SINGLELINE);
 						text_size.cy = min(text_size.cy, free_row_rc.bottom - free_row_rc.top);
 						text_size.cx = rc.right - rc.left;
 
 						// Get rc
-						rc = _GetRectangle(dat, &row_rc, &free_row_rc, &left_pos, &right_pos, left,
-							text_size.cx, text_size.cx, text_size.cy, HORIZONTAL_SPACE);
-
-						if (rc.left < rc.right) {
-							// Store pos
+						rc = _GetRectangle(dat, &row_rc, &free_row_rc, &left_pos, &right_pos, left, text_size.cx, text_size.cx, text_size.cy, HORIZONTAL_SPACE);
+						if (rc.left < rc.right) { // Store pos
 							Drawing->pos_contact_time = rc;
 							_StoreItemPos(Drawing, CIT_TIME, &rc);
 						}
@@ -2205,16 +2183,14 @@ void CLCPaint::_CalcItemsPos(HDC hdcMem, ClcData *dat, ClcContact *Drawing, RECT
 			if (!Drawing->isSubcontact || dat->dbbMetaHideExtra == 0 && dat->extraColumnsCount > 0) {
 				int iImage;
 				int count = 0;
-				RECT rc;
 
 				for (iImage = dat->extraColumnsCount - 1; iImage >= 0; iImage--) {
 					if (Drawing->iExtraImage[iImage] != EMPTY_EXTRA_ICON || !dat->MetaIgnoreEmptyExtra) {
-						rc = _GetRectangle(dat, &row_rc, &free_row_rc, &left_pos, &right_pos,
-							left, dat->extraColumnSpacing, dat->extraColumnSpacing, ICON_HEIGHT, 0);
+						RECT rc = _GetRectangle(dat, &row_rc, &free_row_rc, &left_pos, &right_pos, left, dat->extraColumnSpacing, dat->extraColumnSpacing, ICON_HEIGHT, 0);
 						if (rc.left < rc.right) {
 							// Store position
 							_StoreItemPos(Drawing, CIT_EXTRA | (iImage & 0x3F), &rc);
-							//Drawing->pos_extra[iImage] = rc;
+							// Drawing->pos_extra[iImage] = rc;
 							count++;
 						}
 					}
@@ -2695,15 +2671,15 @@ void CLCPaint::_DrawContactAvatar(HDC hdcMem, ClcData *dat, ClcContact *Drawing,
 					int item = pcli->pfnIconFromStatusMode(Drawing->proto, Drawing->proto == NULL ? ID_STATUS_OFFLINE : GetContactCachedStatus(Drawing->hContact), Drawing->hContact);
 					if (item != -1)
 						_DrawStatusIcon(Drawing, dat, item, hdcMem,
-						ptOverlay.x, ptOverlay.y, ICON_HEIGHT, ICON_HEIGHT,
-						CLR_NONE, CLR_NONE, (blendmode == 255) ? ILD_NORMAL : (blendmode == 128) ? ILD_BLEND50 : ILD_BLEND25);
+							ptOverlay.x, ptOverlay.y, ICON_HEIGHT, ICON_HEIGHT,
+							CLR_NONE, CLR_NONE, (blendmode == 255) ? ILD_NORMAL : (blendmode == 128) ? ILD_BLEND50 : ILD_BLEND25);
 				}
 				break;
 			case SETTING_AVATAR_OVERLAY_TYPE_CONTACT:
 				if (Drawing->iImage != -1)
 					_DrawStatusIcon(Drawing, dat, Drawing->iImage, hdcMem,
-					ptOverlay.x, ptOverlay.y, ICON_HEIGHT, ICON_HEIGHT,
-					CLR_NONE, CLR_NONE, (blendmode == 255) ? ILD_NORMAL : (blendmode == 128) ? ILD_BLEND50 : ILD_BLEND25);
+						ptOverlay.x, ptOverlay.y, ICON_HEIGHT, ICON_HEIGHT,
+						CLR_NONE, CLR_NONE, (blendmode == 255) ? ILD_NORMAL : (blendmode == 128) ? ILD_BLEND50 : ILD_BLEND25);
 				break;
 			}
 		}
@@ -2845,7 +2821,7 @@ void CLCPaint::_DrawContactExtraIcon(HDC hdcMem, ClcData *dat, ClcContact *Drawi
 		_GetBlendMode(dat, Drawing, selected, hottrack, GIM_EXTRAICON_AFFECT, &colourFg, &mode);
 		if (Drawing->iExtraImage[iImage] != EMPTY_EXTRA_ICON)
 			ske_ImageList_DrawEx(dat->himlExtraColumns, Drawing->iExtraImage[iImage], hdcMem,
-			rc->left, rc->top, 0, 0, CLR_NONE, colourFg, mode);
+				rc->left, rc->top, 0, 0, CLR_NONE, colourFg, mode);
 	}
 }
 

plugins/Boltun/src/Engine/Mind.cpp
@@ -80,8 +80,7 @@ vector<tstring> Mind::Parse(tstring s)
 		return res;
 	s.resize(len);
 	int it = 0;
-	while (it != len)
-	{
+	while (it != len) {
 		while (it != len && _istspace(s[it]))
 			it++;
 		if (it == len)
@@ -110,19 +109,15 @@ void Mind::Load(tstring filename)
 
 	bool start = true;
 
-	try
-	{
-		while (file.good())
-		{
+	try {
+		while (file.good()) {
 			getline(file, st);
 			if (st.empty())
 				break;
 			line++;
 
-			if (start)
-			{
-				if (st[0] == 65279)
-				{
+			if (start) {
+				if (st[0] == 65279) {
 					st.erase(0, 1);
 					fileTypeMark = true;
 				}
@@ -136,16 +131,13 @@ void Mind::Load(tstring filename)
 			c = co = new TCHAR[count + 1];
 			mir_tstrcpy(c, st.c_str());
 			size_t pos = 0;
-			while (pos < count && _istspace(*c))
-			{
+			while (pos < count && _istspace(*c)) {
 				++pos;
 				++c;
 			}
 			count -= pos;
-			if (count > 2)
-			{
-				switch (*c)
-				{
+			if (count > 2) {
+				switch (*c) {
 				case '(':
 					if (c[count - 1] != ')')
 						abort();
@@ -160,13 +152,15 @@ void Mind::Load(tstring filename)
 					toLowerStr(c);
 					{
 						WordsList l(c);
-						if (!l.IsEmpty())
+						if (!l.IsEmpty()) {
 							if (l.IsQuestion())
 								data->qkeywords.insert(make_pair(l, s1));
 							else
 								data->keywords.insert(make_pair(l, s1));
+						}
 					}
 					break;
+
 				case '{':
 					if (c[count - 1] != '}')
 						abort();
@@ -188,6 +182,7 @@ void Mind::Load(tstring filename)
 								data->specialEscapes.insert(make_pair(l, s1));
 					}
 					break;
+
 				case '[':
 					if (c[count - 1] != ']')
 						throw error;
@@ -202,6 +197,7 @@ void Mind::Load(tstring filename)
 					toLowerStr(c);
 					data->widelyUsed.insert(make_pair(c, s1));
 					break;
+
 				case '<':
 					if (c[count - 1] != '>')
 						throw error;
@@ -213,62 +209,56 @@ void Mind::Load(tstring filename)
 					++c;
 					count -= 2;
 					c[count] = '\0';
-					if (mir_tstrcmp(c, _T("QUESTION")) == 0)
-					{
+					if (mir_tstrcmp(c, _T("QUESTION")) == 0) {
 						toLowerStr(c);
 						data->question.insert(s1);
 					}
-					else
-						if (mir_tstrcmp(c, _T("IGNORED")) == 0)
-						{
-							toLowerStr(c);
-							data->special.insert(s1);
-						}
-						else
-							if (mir_tstrcmp(c, _T("ESCAPE")) == 0)
-							{
-								data->escape.push_back(s1);
-							}
-							else
-								if (mir_tstrcmp(c, _T("FAILURE")) == 0)
-								{
-									data->failure.push_back(s1);
-								}
-								else
-									if (mir_tstrcmp(c, _T("REPEAT")) == 0)
-									{
-										data->repeats.push_back(s1);
-									}
-									else
-									{
-										if (mir_tstrcmp(c, _T("INITIAL")) != 0)
-											throw error;
-										data->initial.push_back(s1);
-									}
+					else if (mir_tstrcmp(c, _T("IGNORED")) == 0) {
+						toLowerStr(c);
+						data->special.insert(s1);
+					}
+					else if (mir_tstrcmp(c, _T("ESCAPE")) == 0) {
+						data->escape.push_back(s1);
+					}
+					else if (mir_tstrcmp(c, _T("FAILURE")) == 0) {
+						data->failure.push_back(s1);
+					}
+					else if (mir_tstrcmp(c, _T("REPEAT")) == 0) {
+						data->repeats.push_back(s1);
+					}
+					else {
+						if (mir_tstrcmp(c, _T("INITIAL")) != 0)
+							throw error;
+						data->initial.push_back(s1);
+					}
 					break;
+
 				case '@':
-				{
 					if (file.eof())
 						throw error;
+
 					getline(file, s1);
 					line++;
 					format(s1);
 					++c;
 					count -= 1;
 					toLowerStr(c);
-					tstring sc(c);
-					int count1 = (int)s1.length();
-					TCHAR *c = new TCHAR[count1 + 1];
-					mir_tstrcpy(c, s1.c_str());
-					CharLower(c);
-					s1 = c;
-					delete c;
-					vector<tstring> strs = Parse(s1);
-					data->raliases.insert(make_pair(sc, strs));
-					for (vector<tstring>::const_iterator it = strs.begin(); it != strs.end(); ++it)
-						data->aliases.insert(make_pair(*it, sc));
-				}
-				break;
+					{
+						std::transform(s1.begin(), s1.end(), s1.begin(), ::tolower);
+						int count1 = (int)s1.length();
+						TCHAR *c = new TCHAR[count1 + 1];
+						mir_tstrcpy(c, s1.c_str());
+						CharLower(c);
+						s1 = c;
+						delete c;
+						vector<tstring> strs = Parse(s1);
+						tstring sc(c);
+						data->raliases.insert(make_pair(sc, strs));
+						for (vector<tstring>::const_iterator it = strs.begin(); it != strs.end(); ++it)
+							data->aliases.insert(make_pair(*it, sc));
+					}
+					break;
+
 				default:
 					if (file.eof())
 						throw error;
@@ -279,25 +269,21 @@ void Mind::Load(tstring filename)
 					data->study.insert(make_pair(c, s1));
 				}
 			}
-			else
-				if (count)
-				{
-					if (file.eof())
-						throw error;
-					getline(file, s1);
-					line++;
-					format(s1);
-					data->study.insert(make_pair(c, s1));
-				}
+			else if (count) {
+				if (file.eof())
+					throw error;
+				getline(file, s1);
+				line++;
+				format(s1);
+				data->study.insert(make_pair(c, s1));
+			}
 		}
 		if (!file.eof())
-		{
 			throw error;
-		}
+
 		delete co;
 	}
-	catch (...)
-	{
+	catch (...) {
 		throw CorruptedMind(line);
 		delete co;
 	}
@@ -315,80 +301,64 @@ void Mind::Save(tstring filename) const
 	if (fileTypeMark)
 		file << TCHAR(65279);
 
-	for (string_mmap::iterator it = data->study.begin(); it != data->study.end(); ++it)
-	{
+	for (string_mmap::iterator it = data->study.begin(); it != data->study.end(); ++it) {
 		file << (*it).first << _T('\r') << endl;
 		file << (*it).second << _T('\r') << endl;
 	}
-	for (multimap<WordsList, tstring>::iterator it = data->keywords.begin(); it != data->keywords.end(); ++it)
-	{
+	for (multimap<WordsList, tstring>::iterator it = data->keywords.begin(); it != data->keywords.end(); ++it) {
 		file << _T(" (") << (tstring)(*it).first << _T(")") << _T('\r') << endl;
 		file << (*it).second << _T('\r') << endl;
 	}
-	for (multimap<WordsList, tstring>::iterator it = data->qkeywords.begin(); it != data->qkeywords.end(); ++it)
-	{
+	for (multimap<WordsList, tstring>::iterator it = data->qkeywords.begin(); it != data->qkeywords.end(); ++it) {
 		file << _T(" (") << (tstring)(*it).first << _T(")") << _T('\r') << endl;
 		file << (*it).second << _T('\r') << endl;
 	}
-	for (multimap<WordsList, tstring>::iterator it = data->specialEscapes.begin(); it != data->specialEscapes.end(); ++it)
-	{
+	for (multimap<WordsList, tstring>::iterator it = data->specialEscapes.begin(); it != data->specialEscapes.end(); ++it) {
 		file << _T(" {") << (tstring)(*it).first << _T("}") << _T('\r') << endl;
 		file << (*it).second << _T('\r') << endl;
 	}
-	for (multimap<WordsList, tstring>::iterator it = data->qspecialEscapes.begin(); it != data->qspecialEscapes.end(); ++it)
-	{
+	for (multimap<WordsList, tstring>::iterator it = data->qspecialEscapes.begin(); it != data->qspecialEscapes.end(); ++it) {
 		file << _T(" {") << (tstring)(*it).first << _T("}") << _T('\r') << endl;
 		file << (*it).second << _T('\r') << endl;
 	}
-	for (string_mmap::iterator it = data->widelyUsed.begin(); it != data->widelyUsed.end(); ++it)
-	{
+	for (string_mmap::iterator it = data->widelyUsed.begin(); it != data->widelyUsed.end(); ++it) {
 		file << _T(" [") << (*it).first << _T("]") << _T('\r') << endl;
 		file << (*it).second << _T('\r') << endl;
 	}
-	for (set<tstring>::iterator it = data->question.begin(); it != data->question.end(); ++it)
-	{
+	for (set<tstring>::iterator it = data->question.begin(); it != data->question.end(); ++it) {
 		file << _T(" <QUESTION>") << _T('\r') << endl;
 		file << (*it) << _T('\r') << endl;
 	}
-	for (set<tstring>::iterator it = data->special.begin(); it != data->special.end(); ++it)
-	{
+	for (set<tstring>::iterator it = data->special.begin(); it != data->special.end(); ++it) {
 		file << _T(" <IGNORED>") << _T('\r') << endl;
 		file << (*it) << _T('\r') << endl;
 	}
-	for (string_vec::iterator it = data->escape.begin(); it != data->escape.end(); ++it)
-	{
+	for (string_vec::iterator it = data->escape.begin(); it != data->escape.end(); ++it) {
 		file << _T(" <ESCAPE>") << _T('\r') << endl;
 		file << (*it) << _T('\r') << endl;
 	}
-	for (string_vec::iterator it = data->initial.begin(); it != data->initial.end(); ++it)
-	{
+	for (string_vec::iterator it = data->initial.begin(); it != data->initial.end(); ++it) {
 		file << _T(" <INITIAL>") << _T('\r') << endl;
 		file << (*it) << _T('\r') << endl;
 	}
-	for (string_vec::iterator it = data->failure.begin(); it != data->failure.end(); ++it)
-	{
+	for (string_vec::iterator it = data->failure.begin(); it != data->failure.end(); ++it) {
 		file << _T(" <FAILURE>") << _T('\r') << endl;
 		file << (*it) << _T('\r') << endl;
 	}
-	for (string_vec::iterator it = data->repeats.begin(); it != data->repeats.end(); ++it)
-	{
+	for (string_vec::iterator it = data->repeats.begin(); it != data->repeats.end(); ++it) {
 		file << _T(" <REPEAT>") << _T('\r') << endl;
 		file << (*it) << _T('\r') << endl;
 	}
-	for (map<tstring, vector<tstring>>::const_iterator it = data->raliases.begin(); it != data->raliases.end(); ++it)
-	{
+	for (map<tstring, vector<tstring>>::const_iterator it = data->raliases.begin(); it != data->raliases.end(); ++it) {
 		tstring s;
 		const vector<tstring>& v = (*it).second;
 		bool first = true;
-		for (vector<tstring>::const_iterator it1 = v.begin(); it1 != v.end(); ++it1)
-		{
-			if (first)
-			{
+		for (vector<tstring>::const_iterator it1 = v.begin(); it1 != v.end(); ++it1) {
+			if (first) {
 				first = false;
 				s = *it1;
 			}
-			else
-			{
+			else {
 				s += _T(" ") + *it1;
 			}
 		}
@@ -404,8 +374,7 @@ void Mind::LoadSmiles(tstring filename)
 	data->smiles.clear();
 	tstring s;
 	unsigned int l = 0;
-	while (!file.eof())
-	{
+	while (!file.eof()) {
 		getline(file, s);
 		if (s.length() > l)
 			l = (int)s.length();
@@ -420,8 +389,7 @@ void Mind::LoadSmiles(void *smiles, size_t size)
 	TCHAR* buf = (TCHAR*)smiles;
 	unsigned l = 0;
 	TCHAR* end = buf + size;
-	while (buf != end)
-	{
+	while (buf != end) {
 		TCHAR *lend = buf;
 		while (lend != end && *lend != _T('\r'))
 			lend++;

commit 18785a500c0df9ee8ec2384da72c2bb4e1a4777f

Clist_modern: Fix not aligning contact name when there is no avatar (broken in r14980)
Calling _GetRectangle has to be here, because it provides side-effect for positioning contact name correctly.

git-svn-id: http://svn.miranda-ng.org/main/trunk@15091 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

plugins/Clist_modern/src/modern_clcpaint.cpp
@@ -2,7 +2,7 @@
 
 Miranda NG: the free IM client for Microsoft* Windows*
 
-Copyright (ñ) 2012-15 Miranda NG project (http://miranda-ng.org),
+Copyright (ï¿½) 2012-15 Miranda NG project (http://miranda-ng.org),
 Copyright (c) 2000-08 Miranda ICQ/IM project,
 all portions of this codebase are copyrighted to the people
 listed in contributors.txt.
@@ -2056,7 +2056,9 @@ void CLCPaint::_CalcItemsPos(HDC hdcMem, ClcData *dat, ClcContact *Drawing, RECT
 						// Has to keep the empty space??
 						if ((left && !dat->row_align_left_items_to_left) || (!left && !dat->row_align_right_items_to_right)) {
 							// Make rectangle
-							// RECT rc = _GetRectangle(dat, &row_rc, &free_row_rc, &left_pos, &right_pos, left, max_width, max_width, dat->avatars_maxheight_size, HORIZONTAL_SPACE);
+							// NOTE: Calling _GetRectangle on next line has to be here, because it provides side-effect for positioning contact name correctly if contact has no avatar.
+							// It makes it aligned same way as if avatar was here. Just defining rc variable is useless, as it is never used (and just shows warning).
+							/*RECT rc =*/ _GetRectangle(dat, &row_rc, &free_row_rc, &left_pos, &right_pos, left, max_width, max_width, dat->avatars_maxheight_size, HORIZONTAL_SPACE);
 
 							// Store position
 							//StoreItemPos( Drawing, CIT_AVATAR, &rc );